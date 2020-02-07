305c17917655b2cb84805500421ce76e77a75df0
alumno <alumno@utnfra.com.ar>
2020-02-06 22:08:48 -0300

    CLASE 2

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,6 +3,21 @@ Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
-{
+{//variables
+    var importeinicial;
+    var importeinicialparseado;
+    var importecondescuento;
+    var RESULTADO;
+//id
+
+importeinicial=document.getElementById("importe").value;
+
+//operacion
+importeinicialparseado= parseInt (importeinicial);
+importecondescuento= importeinicial *0.25; 
+
+RESULTADO= importeinicialparseado-importecondescuento;
+
+document.getElementById("resultado").value = RESULTADO;
 	
 }
